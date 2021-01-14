d6cf0eedcb6eb37b637d786835ec78f5ee95af03
Nix <64493842+nixKodes@users.noreply.github.com>
2021-01-14 12:09:29 -0600

    Add files via upload

---
+++ GPA.py
@@ -0,0 +1,109 @@
+from __future__ import division
+print ("Welcome to Nix's Unweighted GPA Calulator")
+classes = input('Enter Number of Classes: ')
+
+if classes == "1":
+    class1 = input('GPA of First Class: ')
+    print ('Unweighted GPA:' + class1)
+
+if classes == "2":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class Class: ')
+    total_3 = float(class1) + float(class2) + float(class3)
+    total_d3 = float(total_3) / 3.0
+    print ('Unweighted GPA:' + str(total_d2))
+
+if classes == "3":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class: ')
+    class3 = input('GPA of Third Class: ')
+    total_3 = float(class1) + float(class2) + float(class3)
+    total_d3 = float(total_3) / 3.0
+    print ('Unweighted GPA:' + str(total_d3))
+
+if classes == "4":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class: ')
+    class3 = input('GPA of Third Class: ')
+    class4 = input('GPA of Fourth Class: ')
+    total_4 = float(class1) + float(class2) + float(class3) + float(class4)
+    total_d4 = float(total_4) / 4.0
+    print ('Unweighted GPA:' + str(total_d4))
+
+if classes == "5":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class: ')
+    class3 = input('GPA of Third Class: ')
+    class4 = input('GPA of Fourth Class: ')
+    class5 = input('GPA of Fifth Class: ')
+    total_5 = float(class1) + float(class2) + float(class3) + float(class4) + float(class5) 
+    total_d5 = float(total_5) / 5.0
+    print ('Unweighted GPA:' + str(total_d5))
+
+if classes == "6":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class: ')
+    class3 = input('GPA of Third Class: ')
+    class4 = input('GPA of Fourth Class: ')
+    class5 = input('GPA of Fifth Class: ')
+    class6 = input('GPA of Sixth Class: ')
+    total_6 = float(class1) + float(class2) + float(class3) + float(class4) + float(class5) + float(class6) 
+    total_d6 = float(total_6) / 6.0
+    print ('Unweighted GPA:' + str(total_d6))
+
+if classes == "7":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class: ')
+    class3 = input('GPA of Third Class: ')
+    class4 = input('GPA of Fourth Class: ')
+    class5 = input('GPA of Fifth Class: ')
+    class6 = input('GPA of Sixth Class: ')
+    class7 = input('GPA of Seventh Class: ')
+    total_7 = float(class1) + float(class2) + float(class3) + float(class4) + float(class5) + float(class6) + float(class7) 
+    total_d7 = float(total_7) / 7.0
+    print ('Unweighted GPA:' + str(total_d7))
+
+if classes == "8":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class: ')
+    class3 = input('GPA of Third Class: ')
+    class4 = input('GPA of Fourth Class: ')
+    class5 = input('GPA of Fifth Class: ')
+    class6 = input('GPA of Sixth Class: ')
+    class7 = input('GPA of Seventh Class: ')
+    class8 = input('GPA of Eighth Class: ')
+    total_8 = float(class1) + float(class2) + float(class3) + float(class4) + float(class5) + float(class6) + float(class7) + float(class8) 
+    total_d8 = float(total_8) / 8.0
+    print ('Unweighted GPA:' + str(total_d8))
+
+if classes == "9":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class: ')
+    class3 = input('GPA of Third Class: ')
+    class4 = input('GPA of Fourth Class: ')
+    class5 = input('GPA of Fifth Class: ')
+    class6 = input('GPA of Sixth Class: ')
+    class7 = input('GPA of Seventh Class: ')
+    class8 = input('GPA of Eighth Class: ')
+    class9 = input('GPA of Ninth Class: ')
+    total_9 = float(class1) + float(class2) + float(class3) + float(class4) + float(class5) + float(class6) + float(class7) + float(class8) + float(class9) 
+    total_d9 = float(total_9) / 9.0
+    print ('Unweighted GPA:' + str(total_d9))
+
+if classes == "10":
+    class1 = input('GPA of First Class: ')
+    class2 = input('GPA of Second Class: ')
+    class3 = input('GPA of Third Class: ')
+    class4 = input('GPA of Fourth Class: ')
+    class5 = input('GPA of Fifth Class: ')
+    class6 = input('GPA of Sixth Class: ')
+    class7 = input('GPA of Seventh Class: ')
+    class8 = input('GPA of Eighth Class: ')
+    class9 = input('GPA of Ninth Class: ')
+    class10 = input('GPA of Tenth Class: ')
+    total_10 = float(class1) + float(class2) + float(class3) + float(class4) + float(class5) + float(class6) + float(class7) + float(class8) + float(class9) + float(class10)
+    total_d10 = float(total_10) / 10.0
+    print ('Unweighted GPA:' + str(total_d10))
+
+#nixKodes num.004
+
